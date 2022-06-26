3649f5d71f19276f8a23368a7264dfdbf95b4812
nisa_ashfiyani <ashfii.yaa22@gmail.com>
2022-06-26 10:12:03 +0700

    first commit

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
  //koneksi ke database surat
- $server = "localhost";
-  $user = "root";
- $pass = "";
- $dbname = "surat";
+ $server = "host=tiny.db.elephantsql.com port=5432";
+  $user = "mgxmwkey";
+ $pass = "u0BT3wGIRQ1idfXBIa-79rTkfmdycJYJ";
+ $dbname = "mgxmwkey";
 
  $koneksi = pg_connect($server,$user,$pass,$dbname);
  if(pg_connect_errno()){
